0199891d5c5b5511ff45c683fdcd235077aa4754
Alexander Simakov <a.simakov.hh@gmail.com>
2022-08-09 15:35:34 +0200

    started

---
+++ exercise.py
@@ -0,0 +1,121 @@
+# do exercise here
+# upload to github for portfolio
+# thank me later when your git graph is as green as the python money u gonna earn
+'''1'''
+#print(int(234))
+#print(float(43.12))
+#print(complex(3+2j))
+#print("hello")
+#print(bool(True))
+"""2"""
+
+
+#a = 123
+#b = type(a)
+#print(a,b,sep=" is type of ")
+
+
+
+#a = 43.23
+#b = type(a)
+#print(a,b,sep=" is type of ")
+
+
+
+#a = (4-1j)
+#b = type(a)
+#print(a,b,sep=" is type of ")#
+
+
+#a = "How are you?"
+#b = type(a)
+#print(a,b,sep=" is type of ")
+
+#a = True
+#b = type(a)
+#print(a,b,sep=" is type of ")
+
+"""3"""
+
+
+
+
+"""
+a = 123
+b = isinstance(a, int)
+print(a,b)
+
+
+a = 42.23
+b = isinstance(a, float)
+print(a,b)
+
+a = True
+b = isinstance(a, bool)
+print(a,b)
+
+a = (4+2j)
+b = isinstance(a, complex)
+print(a,b)
+
+a = "hello"
+b = isinstance(a, str)
+print(a,b)
+"""
+
+
+"""4"""
+
+
+#a = 123
+#b = int
+#c = isinstance(a, b)
+#print("is",a, "in an instance of:",b,"?",c)
+
+
+""""
+a = 123
+b = int
+c = isinstance(a, b)
+print("is",a, "in an instance of bool?:",c)
+
+a = 42.23
+b = bool
+c = isinstance(a, b)
+print("is",a, "in an instance of float?",c)
+
+a = (4-1j)
+b = complex
+c = isinstance(a, b)
+print("is",a, "in an instance of complex?",c)
+
+a = True
+b = bool
+c = isinstance(a, b)
+print("is",a, "in an instance of bool",c)
+
+
+a = "how are you?"
+b = float
+c = isinstance(a, b)
+print("is",a, "in an instance of float",c)
+"""
+
+
+
+
+
+
+
+"""5"""
+
+#a = 5 #variable
+
+"""comment
+comment
+comment"""
+
+
+"""5"""
+
+#testcomment
